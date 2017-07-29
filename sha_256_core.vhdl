0c48186cbf591c00a688dea4ed4b1749c79b8588
dsaves <dronin227@gmail.com>
2017-07-29 08:58:02 -0600

    Cleaned up file declarations.

--- sha_256_core.vhdl
+++ sha_256_core.vhdl
@@ -38,18 +38,17 @@ use work.sha_256_pkg.all;
 
 entity sha_256_core is
     generic(
-        RESET_VALUE : std_logic := '0';    --reset enable value
-        WORD_WIDTH : NATURAL    := 32    --sha256 uses 32-bit words
+        RESET_VALUE : std_logic := '0'    --reset enable value
     );
     port(
         clk : in std_logic;
         rst : in std_logic;
         data_ready : in std_logic;  --the edge of this signal triggers the capturing of input data and hashing it.
         n_blocks : in natural; --N, the number of (padded) message blocks
-        msg_block_in : in std_logic_vector(0 to (16 * WORD_WIDTH)-1);
+        msg_block_in : in std_logic_vector(0 to (16 * WORD_SIZE)-1);
         --mode_in : in std_logic;
         finished : out std_logic;
-        data_out : out std_logic_vector((WORD_WIDTH * 8)-1 downto 0) --SHA-256 results in a 256-bit hash value
+        data_out : out std_logic_vector((WORD_SIZE * 8)-1 downto 0) --SHA-256 results in a 256-bit hash value
     );
 end entity;
 
@@ -60,19 +59,62 @@ architecture sha_256_core_ARCH of sha_256_core is
     constant HASH_02_COUNT_LIMIT : natural := 64;
     
     --Temporary words
-    signal T1 : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal T2 : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
+    signal T1 : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal T2 : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
 
     --Working variables, 8 32-bit words
-    signal a : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal b : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal c : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal d : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal e : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal f : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal g : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
-    signal h : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
+    signal a : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal b : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal c : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal d : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal e : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal f : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal g : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
+    signal h : std_logic_vector(WORD_SIZE-1 downto 0) := (others => '0');
     
+    constant K : K_DATA := (
+        --address 0
+        X"428a2f98", X"71374491", X"b5c0fbcf", X"e9b5dba5",
+        X"3956c25b", X"59f111f1", X"923f82a4", X"ab1c5ed5",
+        X"d807aa98", X"12835b01", X"243185be", X"550c7dc3",
+        X"72be5d74", X"80deb1fe", X"9bdc06a7", X"c19bf174",
+        X"e49b69c1", X"efbe4786", X"0fc19dc6", X"240ca1cc",
+        X"2de92c6f", X"4a7484aa", X"5cb0a9dc", X"76f988da",
+        X"983e5152", X"a831c66d", X"b00327c8", X"bf597fc7",
+        X"c6e00bf3", X"d5a79147", X"06ca6351", X"14292967",
+        X"27b70a85", X"2e1b2138", X"4d2c6dfc", X"53380d13",
+        X"650a7354", X"766a0abb", X"81c2c92e", X"92722c85",
+        X"a2bfe8a1", X"a81a664b", X"c24b8b70", X"c76c51a3",
+        X"d192e819", X"d6990624", X"f40e3585", X"106aa070",
+        X"19a4c116", X"1e376c08", X"2748774c", X"34b0bcb5",
+        X"391c0cb3", X"4ed8aa4a", X"5b9cca4f", X"682e6ff3",
+        X"748f82ee", X"78a5636f", X"84c87814", X"8cc70208",
+        X"90befffa", X"a4506ceb", X"bef9a3f7", X"c67178f2"
+    );
+    
+    --Message schedule, W(00), W(01), ...W(63) (64 32-bit words)
+    signal W : K_DATA := (
+        --address 0
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000",
+        X"00000000", X"00000000", X"00000000", X"00000000"
+    );
+    
+    --Message blocks, the padded message should be a multiple of 512 bits,
+    signal M : M_DATA;
     --Hash values w/ initial hash values; 8 32-bit words
     signal HV : H_DATA;
     signal HV_INITIAL_VALUES : H_DATA := (X"6a09e667", X"bb67ae85", X"3c6ef372",
@@ -104,7 +146,7 @@ begin
     
     
     --next state logic
-    process(CURRENT_STATE, HASH_ROUND_COUNTER, HASH_02_COUNTER, rst, data_ready)
+    process(CURRENT_STATE, rst, n_blocks, HASH_ROUND_COUNTER, HASH_02_COUNTER, data_ready)
     begin
         case CURRENT_STATE is
             when RESET =>
@@ -237,8 +279,8 @@ begin
     MESSAGE_BLOCK_INTERMEDIATE :
     for i in 0 to 15 generate
     begin
-        --M_INT(i) <= msg_block_in((WORD_WIDTH * (i+1))-1 downto WORD_WIDTH * i);
-        M_INT(i) <= msg_block_in((WORD_WIDTH * i) to WORD_WIDTH * (i+1)-1);
+        --M_INT(i) <= msg_block_in((WORD_SIZE * (i+1))-1 downto WORD_SIZE * i);
+        M_INT(i) <= msg_block_in((WORD_SIZE * i) to WORD_SIZE * (i+1)-1);
     end generate;
     
     MESSAGE_SCHEDULE_INTERMEDIATE_00:
--- sha_256_pkg.vhdl
+++ sha_256_pkg.vhdl
@@ -33,53 +33,11 @@ use ieee.numeric_std.all;
 package sha_256_pkg is
     constant WORD_SIZE : natural := 32;
     
+    --array types for SHA-256
     type K_DATA is array (0 to 63) of std_logic_vector(WORD_SIZE-1 downto 0);
     type M_DATA is array (0 to 15) of std_logic_vector(WORD_SIZE-1 downto 0);
     type H_DATA is array (0 to 7) of std_logic_vector(WORD_SIZE-1 downto 0);
     
-    constant K : K_DATA := (
-        --address 0
-        X"428a2f98", X"71374491", X"b5c0fbcf", X"e9b5dba5",
-        X"3956c25b", X"59f111f1", X"923f82a4", X"ab1c5ed5",
-        X"d807aa98", X"12835b01", X"243185be", X"550c7dc3",
-        X"72be5d74", X"80deb1fe", X"9bdc06a7", X"c19bf174",
-        X"e49b69c1", X"efbe4786", X"0fc19dc6", X"240ca1cc",
-        X"2de92c6f", X"4a7484aa", X"5cb0a9dc", X"76f988da",
-        X"983e5152", X"a831c66d", X"b00327c8", X"bf597fc7",
-        X"c6e00bf3", X"d5a79147", X"06ca6351", X"14292967",
-        X"27b70a85", X"2e1b2138", X"4d2c6dfc", X"53380d13",
-        X"650a7354", X"766a0abb", X"81c2c92e", X"92722c85",
-        X"a2bfe8a1", X"a81a664b", X"c24b8b70", X"c76c51a3",
-        X"d192e819", X"d6990624", X"f40e3585", X"106aa070",
-        X"19a4c116", X"1e376c08", X"2748774c", X"34b0bcb5",
-        X"391c0cb3", X"4ed8aa4a", X"5b9cca4f", X"682e6ff3",
-        X"748f82ee", X"78a5636f", X"84c87814", X"8cc70208",
-        X"90befffa", X"a4506ceb", X"bef9a3f7", X"c67178f2"
-    );
-    
-    --Message schedule, W(00), W(01), ...W(63) (64 32-bit words)
-    signal W : K_DATA := (
-        --address 0
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000",
-        X"00000000", X"00000000", X"00000000", X"00000000"
-    );
-    
-    --Message blocks, the padded message should be a multiple of 512 bits,
-    signal M : M_DATA;
     
     --function definitions
     function ROTR (a : std_logic_vector(WORD_SIZE-1 downto 0); n : natural)
